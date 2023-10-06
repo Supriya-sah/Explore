5366e92d807bf200b2a1367ca3440d23127ba6bb
supriya sah <supriyasah1211@gmail.com>
2023-10-06 22:47:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// var a=document.querySelector("h1");
+// console.log(a);
+// a.innerHTML="Hi Supriya sah";
+// var e = document.querySelector("h1");
+// e.innerHTML="ab hoga jadu"
+// e.style.backgroundColor="pink"
+// e.style.color="purple"
+// e.addEventListener("mouseenter", function () {
+//   e.innerHTML = "ab hoga jadu";
+//   e.style.backgroundColor = "pink";
+//   e.style.color = "purple";
+// });
+// var light = document.querySelector(".bulb");
+// var btn = document.querySelector("button");
+// var flag = 0;
+// btn.addEventListener("click", function () {
+//   if (flag == 0) {
+//     light.style.backgroundColor = "pink";
+//     btn.innerHTML = "chnaged";
+//     flag=1
+//   }else{
+//     light.style.backgroundColor="black"
+//     btn.innerHTML="off"
+//     flag=0
+//   }
+// });()
+// var a=document.querySelector("btn")
+// a.addEventListener("click",function(){
+//     a.innerHTML="jyfjyhfj";
+// }
+
+var a=document.querySelector("#btn")
+a.addEventListener("click",function(){
+    a.innerHTML="Hawa Mahal means the 'Palace of Winds' or the 'Wind Palace'"
+    a.style.backgroundColor="lightblue"
+    a.style.color="purple"
+    a.style.fontFamily="cursive"
+    a.style.border="2px solid black"
+})
